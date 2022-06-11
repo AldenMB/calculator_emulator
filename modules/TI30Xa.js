ddbe5778b5f22fbb4c4186532743c28d7bb33490
Alden Bradford <aldenbradford@gmail.com>
2022-06-11 12:15:35 -0400

    allow binary operations to recurse along stack

--- modules/TI30Xa.js
+++ modules/TI30Xa.js
@@ -661,10 +661,10 @@ function TI30Xa_state(changes){
 				if(binary_op_precedence(op1) >= binary_op_precedence(op2)){
 					const stack = state.stack.slice(0,-4);
 					stack.push(apply_binary_op(a, op1, b), op2);
-					return child({stack});
+					return child({stack}).reduce_binary_op();
 				} else {
 					return state;
-				};				
+				};		
 			};
 		};
 		
