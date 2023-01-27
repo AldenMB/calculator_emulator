4917db24969c87c97912937c943c4f215438acfe
Alden Bradford <aldenbradford@gmail.com>
2023-01-27 17:23:57 -0500

    show more conflicts at once

--- xanthippe/findconflicts.js
+++ xanthippe/findconflicts.js
@@ -25,7 +25,7 @@ function calcTree(state = TI30Xa().now()){
 
 let conflicts = 0;
 let successes = 0;
-const maxConflicts = 4;
+const maxConflicts = 8;
 const exclusions = ['SIN', 'COS', 'TAN'];
 const calc = calcTree();
 
