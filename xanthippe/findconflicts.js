8c728bf64e5e5a1f6b6e7f14bcdecfe160f31799
Alden Bradford <aldenbradford@gmail.com>
2023-02-10 15:10:54 -0500

    cache conflicts for faster testing

--- xanthippe/findconflicts.js
+++ xanthippe/findconflicts.js
@@ -3,6 +3,9 @@
 import {TI30Xa, array_equal} from '../modules/TI30Xa.js';
 import {run_all_tests} from '../tests/test.js';
 import {loadDatabase, iterDatabase} from './fetch.js';
+import fs from 'fs';
+
+const cache = JSON.parse(fs.readFileSync('./cache.json'));
 
 console.log(run_all_tests().message);
 
@@ -21,14 +24,32 @@ function ignore_paren_indicator(screen1, screen2){
 	return screen1.slice(0, 73) === screen2.slice(0, 73) && screen1.slice(75) === screen2.slice(75);
 }
 
+function addToCache(sequence, screen){
+	if( !cache.some( testcase => (
+		testcase[0].length === sequence.length && 
+		testcase[0].every((x, i) => x === sequence[i])
+		))
+	){
+		cache.push([sequence, screen]);
+		fs.writeFileSync('./cache.json', JSON.stringify(cache));
+	}
+}
+
+async function* testCases(){
+	console.log('Working through cache...');
+	yield* cache;
+	console.log('Loading new cases...');
+	yield* iterDatabase();
+}
+
 let conflicts = 0;
 let successes = 0;
 let skipped = 0;
 const maxConflicts = 10;
-const exclusions = ['ab/c', 'Sigma+'];
+const exclusions = ['Sigma+'];
 
 console.log('searching...');
-for await (const [sequence, screen] of iterDatabase()){
+for await (const [sequence, screen] of testCases()){
 	process.stdout.write(`${conflicts}/${successes}/${skipped}`);
 	if(sequence.some(x => exclusions.includes(x))){
 		skipped += 1;
@@ -55,6 +76,8 @@ for await (const [sequence, screen] of iterDatabase()){
 			console.log('Measured:');
 			console.log(screen);
 			console.log('');
+			
+			addToCache(sequence, screen);
 		}
 		if (conflicts > maxConflicts){
 			break;
